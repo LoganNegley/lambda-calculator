1f7defc281276c8bd1376c2450e50ba06dd453ac
Logan Negley <logannegley@gmail.com>
2019-11-15 21:25:41 -0600

    exported all data.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -4,7 +4,8 @@ import Numbers from "./Numbers";
 const NumberButton = (props) => {
   return (
     <button>
-{props.number}
+    <div>{props.number}</div>
+
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </button>
   );
