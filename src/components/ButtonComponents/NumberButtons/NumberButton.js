01855d665a79e101e1e6b8bb5933b5201753039d
Logan Negley <logannegley@gmail.com>
2019-11-15 20:28:19 -0600

    nothing changed

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Numbers from "./Numbers";
 
 const NumberButton = () => {
   return (
-    <>
+    <button>
+
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
 
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -11,10 +11,15 @@ import { numbers } from '../../../data'
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numbers, setNumbers] = useState(numbers);
-  
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
+      {
+        numbers.map((num, index)=>{
+
+      })
+        
+        /* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
